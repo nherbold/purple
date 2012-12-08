ae691effd79ec0d64747a6932fe708779adb1b52
Nick Herbold <herboldn@gmail.com>
2012-12-08 16:02:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purple::Application.config.secret_token = '18a88b91ec10db88b1f67a5a9e007222f6a2220e73ecd57f9ef23d0cb5bf85fb69f22f3d808bccd2b15f0f9975b8fbd889ceb1b0f805e823589e9baa643d67c8'
